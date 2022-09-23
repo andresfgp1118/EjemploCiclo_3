80d7cffbddecb6e4abfa8ee26ceb74317b599c28
Andres <andresfgp1118@gmail.com>
2022-09-22 21:26:59 -0500

    Ejercicios varios

---
+++ pruebajs.js
@@ -0,0 +1,10 @@
+function function1() {
+    
+    let x = 5;
+    let a = prompt("digite el valor")
+    let b = prompt("digite el valor")
+    let c = parseInt(a)+parseInt(b);
+    let y = 6;
+    let z = x + y;
+    document.getElementById("prueba").innerHTML = "Hola Mundooo parrafo " + c;
+    }
